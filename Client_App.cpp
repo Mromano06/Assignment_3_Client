9c06b48168fe699435c5217264a8136b3d3a2e6d
Matthew <matbat2205@gmail.com>
2025-12-01 16:35:01 +0000

    Added part of the client implementation

--- Client_App.cpp
+++ Client_App.cpp
@@ -1,7 +1,35 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
 // Matthew Romano - Brodie Arkell - Assignment 3 - Network Theory
 // Client side apllication implementaion
 
-int main(void) {
+int main() {
+    
+    // Create the socket
+    int cSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (cSocket < 0) {
+        std::cerr << "ERROR: Failed to create socket" << std::endl;
+        return -1;
+    }
 
-    return 0;
+    // Define the servers address
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2222); // TODO: Decide on port number used
+    server_addr.sin_addr.s_addr = inet_addr("192.168.0.0"); // TODO: Get the ip address of the server
+
+    // Connect to the server
+    if (connect(cSocket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "ERROR: Failed to connect to server" << std::endl;
+        close(cSocket);
+        return -1;
+    }
+    std::cout << "Server connection established." << std::endl;
+
 }
+
