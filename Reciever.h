9dbf42498c49f569ef035f75c7a5127797593285
Matthew <matbat2205@gmail.com>
2025-12-02 01:28:22 +0000

    Added reciever to parse maessage from server

---
+++ Reciever.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+// Matthew Romano - Assignment 3 - Network Theory
+// Reciever class declaration
+
+class Reciever {
+    public:
+        std::string checkCommands(char* buffer);     
+};
